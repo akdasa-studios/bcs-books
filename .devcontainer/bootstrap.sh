bf5a9aa478550ebfa8cdce603480a1f754dd12cf
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2022-11-13 11:33:14 +0000

    playwright

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-# fish config
-mkdir -p ~/.config/fish
-cp ./.devcontainer/config.fish ~/.config/fish/
-
 # ionic dev tools
 npm install -g @ionic/cli@latest
 npm install -g capacitor-resources
 npm install -g eslint
-# npm install -g stryker-cli
-# npm install -g cypress
 
 # project dependencies
 npm install
--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,5 @@ module.exports = {
   transformIgnorePatterns: ['/node_modules/(?!@ionic/vue|@ionic/vue-router|@ionic/core|@stencil/core|ionicons)'],
   testPathIgnorePatterns: [
     "/node_modules/",
-    ".stryker-tmp/"
   ]
 }
