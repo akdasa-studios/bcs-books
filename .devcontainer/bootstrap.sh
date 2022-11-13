1b7423bd4c9ae793720dcd56ecd5293af7fd54e1
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2022-11-13 12:43:53 +0000

    test

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -4,6 +4,7 @@
 npm install -g @ionic/cli@latest
 npm install -g capacitor-resources
 npm install -g eslint
+npm install -g playwright && playwright install && playwright install-deps
 
 # project dependencies
 npm install
